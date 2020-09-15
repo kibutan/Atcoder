63ca59b9b9b176b9b2dc6dd69f9350530296e75b
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2020-09-15 13:35:38 +0900

    Update C - Ubiquity.py
    It (may) works perfectly.

--- ABC178/C - Ubiquity.py
+++ ABC178/C - Ubiquity.py
@@ -1,9 +1,11 @@
 N = int(input())
-temp = [0 for m in range(N)]
-cnt = 0
-for j in range(N):
-  for i in range(N):
-    for a in range(1,9,1):
-      temp[i] = a
-      if temp[i] == 0&temp[j]==9: cnt = cnt + 1
-print(cnt)
+mod = 1000000007
+def powmod(x,y):
+  res = 1
+  for i in range(y):
+    res = res*x%mod
+  return res
+ans = powmod(10,N)-powmod(9,N)-powmod(9,N)+powmod(8,N)
+ans = ans%mod
+ans = (ans + mod)%mod
+print(ans)
