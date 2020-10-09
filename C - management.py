43ce8a7fdf6f395900722d6e80ac9cb69ce87683
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2020-10-09 14:38:23 +0900

    Create C - management.py

---
+++ C - management.py
@@ -0,0 +1,17 @@
+import collections
+N = int(input())
+A = list(map(int,input().split()))
+cnt = [0]*N
+if(A[])
+
+
+
+#for i in range (N-1):
+#  if(i+1 < A[N-1]):
+#    if(A[i]==A[i+1]):
+#      cnt[N-1] = cnt[N-1] + 1
+#      continue
+#  else:cnt[A[N-1]]
+#print(cnt)
+    #  print(A.count(i+1))
+#print(0)
