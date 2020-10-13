386f9c89002913050fa085786453eb7783a30294
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2020-10-13 10:45:29 +0900

    Update B - MAX-=min.py
    it works correctly

--- ARC105/B - MAX-=min.py
+++ ARC105/B - MAX-=min.py
@@ -1,9 +1,7 @@
+import math
+from functools import reduce
+def gcd(*numbers):
+  return reduce(math.gcd, numbers)
 N = int(input())
 A = list(map(int,input().split()))
-while(min(A) != max(A)):
-  maxIndex = [i for i, x in enumerate(A) if x == max(A)]
-  for i in range(len(maxIndex)):
-    A[maxIndex[i]] = A[maxIndex[i]] - min(A)
-  if(min(A) == max(A)):break
-print(A[0])
-#最大値のIndexを返す関数MaxIndex
+print(gcd(*A))
