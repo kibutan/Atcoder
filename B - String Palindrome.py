d516bb49f59003e66bf18e2f82ca69f23fa0dfae
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2020-10-25 14:59:09 +0900

    Create B - String Palindrome.py

---
+++ B - String Palindrome.py
@@ -0,0 +1,12 @@
+import math
+S = input()
+ans = "No"
+center=math.ceil(len(S)/2) #中央の文字数
+S_1st = S[0:center-1]
+S_2nd = S[center:len(S)]
+
+if(S_1st == S_2nd):
+  if(S_1st[::-1] == S_1st):
+    ans = "Yes"
+
+print(ans)
