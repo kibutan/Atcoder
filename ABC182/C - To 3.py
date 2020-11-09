67d6c703f79f5c8061b4bae618498fcef4747f98
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2020-11-09 09:25:14 +0900

    Update C - To 3.py

--- ABC182/C - To 3.py
+++ ABC182/C - To 3.py
@@ -1,24 +1,27 @@
 def digitSum(n):
     # 数値を文字列に変換
-#    s = str(n)
+    s = str(n)
     # １文字ずつ数値化し配列にする。
-    array = list(map(int, n))
+    array = list(map(int, s))
     # 合計値を返す
+#    print("sum:"+str(sum(array)))
     return sum(array)
-N = input()
+
+N = int(input())
+S = str(N)
 ans = 0
-if(len(N)==1):
-  if(int(N)%3 == 0):
+if(len(str(N))==1):
+  if(N%3 == 0):
     print(0)
     exit()
   else:
     print(-1)
     exit()
-if(len(N) == 2):
-  if(int(N)%3 == 0):
+if(len(S) == 2):
+  if(N%3 == 0):
     print(0)
     exit()
-  elif(int(N[0])%3 ==0 or int(N[1])%3 ==0):
+  elif(int(S[0])%3 ==0 or int(S[1])%3 ==0):
     print(1)
     exit()
   else:
@@ -29,12 +32,12 @@ if(digitSum(N)%3 == 0):
   print(0)
   exit()
 elif(digitSum(N)%3 == 1):
-#  print(1)
-  if(1 or 4 or 7) in N:ans = 1
+#  print("1" in S or "4" in S or "7" in S)
+  if("1" in S or "4" in S  or "7" in S):ans = 1
   else: ans = 2
   print(ans)
 elif(digitSum(N)%3 == 2):
-#  print(2)
-  if(2 or 5 or 8) in N:ans = 1
+#  print("2" in S or "5" in S or "8" in S)
+  if("2" in S or "5" in S or "8" in S):ans = 1
   else: ans = 2
   print(ans)
