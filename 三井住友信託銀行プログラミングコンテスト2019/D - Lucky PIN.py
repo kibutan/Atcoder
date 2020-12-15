bf96100e7a3341e5eb71e6cb27ac3f41cdbc998f
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2020-12-15 11:46:18 +0900

    Update D - Lucky PIN.py

--- 三井住友信託銀行プログラミングコンテスト2019/D - Lucky PIN.py
+++ 三井住友信託銀行プログラミングコンテスト2019/D - Lucky PIN.py
@@ -1,16 +1,33 @@
-#TLE
-
+#AC
 N = int(input())
 S = input()
-i_list =[]
-j_list =[]
-k_list =[]
-PIN_list = []
+index_i=0
+index_j=0
+index_k=0
 
-for i in range(0,N-2):
-  for j in range(i+1,N-1):
-    for k in range(j+1,N):
-      if(S[i]+S[j]+S[k] in PIN_list):continue
-      else:PIN_list.append(S[i]+S[j]+S[k])
+ans = 0
+PIN_list =[]
+
+for i in range(10):
+  index_i = S[:N-2].find(str(i))
+#  print("i"+str(i))
+#  print("index_i:"+str(index_i))
+  if index_i != -1 :
+    for j in range(10):
+      index_j = S[index_i+1:N-1].find(str(j))+index_i+1
+#      print("j"+str(j))
+#      print("index_j:"+str(index_j))
+      if (index_j != -1 and index_i < index_j):
+#        index_j += index_i + 1
+        for k in range(10):
+          index_k = S[index_j+1:].find(str(k))+index_j+1
+#          print("k"+str(k))
+#          print("index_k:"+str(index_k))
+          if (index_k != -1 and index_j < index_k):
+#            index_k += index_j + 1
+#            print("index_i"+ str(index_i) +"index_j"+ str(index_j) +"index_k"+ str(index_k))
+#            print(str(i)+str(j)+str(k))
+            PIN_list.append(str(i)+str(j)+str(k))
+            ans+=1 
+print(ans)
 #print(PIN_list)
-print(len(PIN_list))
