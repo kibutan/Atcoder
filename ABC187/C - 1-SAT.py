8b1a0666276bcd196a2e0709a30bd5be64a413f8
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2021-01-27 08:17:18 +0900

    Update C - 1-SAT.py

--- ABC187/C - 1-SAT.py
+++ ABC187/C - 1-SAT.py
@@ -1,13 +1,8 @@
 n  = int(input())
-s = [input() for _ in range(n)]
+s = set([input() for _ in range(n)])
 
-s_set = sorted(list(set(s)),reverse = True)
-print(s_set)
-
-for i in range(len(s_set)):
-  if( "!"+s_set[i] in s_set[i:]):
-    print(s_set[i])
+for i in s:
+  if( "!"+i in s):
+    print(i)
     exit()
-  else:continue
-print("satisfiable")
-  
+print("satisfiable")  
