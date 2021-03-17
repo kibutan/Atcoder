280ff485b355f92e5a18705ea3aad1641a6ca766
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2021-03-17 15:51:43 +0900

    Create C - Buy an Integer.py

---
+++ ABC146/C - Buy an Integer.py
@@ -0,0 +1,14 @@
+a,b,x = list(map(int,input().split()))
+def is_ok(n):
+  return a * n + b * len(str(n)) <= x
+
+def meguru_bisect(ng,ok):
+  while (abs(ng-ok) > 1):
+    mid = (ok+ng)//2
+    if is_ok(mid):
+      ok = mid
+    else:
+      ng = mid
+  return ok
+
+print(meguru_bisect(10**9+1,0))
