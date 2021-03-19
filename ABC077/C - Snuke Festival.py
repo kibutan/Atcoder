af1e0ebfd6a997ec4f890aa1f83060f39ef494a5
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2021-03-19 11:26:04 +0900

    Update C - Snuke Festival.py
    AC

--- ABC077/C - Snuke Festival.py
+++ ABC077/C - Snuke Festival.py
@@ -1,51 +1,15 @@
 import bisect
-import collections
-
 N = int(input())
 A = list(map(int,input().split()))
 A.sort()
-print(A)
 B = list(map(int,input().split()))
-B.sort()
-print(B)
 C = list(map(int,input().split()))
-#C.sort()
-#print(C)
-
+C.sort()
 ans = 0
-ab = []
-bc = []
-for j in range(N):
-  if(bisect.bisect_left(B,C[j])!=0):
-    bc.append(bisect.bisect_left(B,C[j]))
-  if(bisect.bisect_left(A,B[j])!=0):
-    ab.append(bisect.bisect_left(A,B[j]))
-print(bc)
-print(ab)
-# BとAの関係も辞書かしたほうが早いのでは？
-cnt_bc = collections.Counter(bc)
-cnt_ab = collections.Counter(ab)
-
-print(cnt_bc)
-print(cnt_ab)
-
-#for i in bc:
-#  ans += bisect.bisect_left(A,B[i-1])
-#print(ans)
-for j in cnt_bc:
-#  print("j",j)
-  for i in range(j):
-#    print(j)
-#    print("i:",i," bug:",(bisect.bisect_left(A,B[j-1]-1-i)))
-#    print("B[",j-1-i,"](",B[j-1-i],")以下のAの数",bisect.bisect_left(A,B[j-1-i]))
-    ans += (bisect.bisect_left(A,B[j-1-i]) * cnt_bc[j])
-
-#for j in range(N):
-#  print("C[j]",C[j])
-#  print("C[j]以下のBの数",bisect.bisect_left(B,C[j]))
-#  if(bisect.bisect_left(B,C[j])!=0):
-#    for i in range(bisect.bisect_left(B,C[j])):
-#    print("i:",i," bug:",(bisect.bisect_left(B,C[j]))-1-i)
-#    print("C[J]以下のB[？]以下のAの数",bisect.bisect_left(A,B[bisect.bisect_left(B,C[j])-1-i]))
-#      ans += (bisect.bisect_left(A,B[bisect.bisect_left(B,C[j])-1-i]))
+ab = 0
+bc = 0
+for i in range(N):
+    bc = (bisect.bisect_right(C,B[i]))
+    ab = (bisect.bisect_left(A,B[i]))
+    ans += ab*(len(C) - bc)
 print(ans)
