0f959414024f558110e6301972986f36e753eb5f
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2021-03-24 08:51:28 +0900

    Update C - Tax Increase.py

--- ABC158/C - Tax Increase.py
+++ ABC158/C - Tax Increase.py
@@ -1,13 +1,6 @@
-def solve(a,b):
-  x = (b - a)*50
-#  print(x)
-  if(x != 0 and int(x * 0.08) == a and int(x * 0.1) ==b):return int(x)
-  elif(x == 0):
-    if(a == 0):return 9
-    elif(a == 1):return 13
-    elif(a == 2):return 25
-    elif(a == 3):return 38
-    else: return -1
-  else:return -1
 a,b = list(map(int,input().split()))
-print(solve(a,b))
+ans = float("inf")
+for i in range(1009):
+  if(int(i * 8/100) == a and int(i * 10/100) == b):ans = min(ans, i)
+if(ans != float("inf")):print(ans)
+else:print(-1)
