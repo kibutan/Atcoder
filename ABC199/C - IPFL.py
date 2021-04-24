d4bf14b565ea788e526cbd9b2d423ed0852b0efe
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2021-04-24 22:28:56 +0900

    Update C - IPFL.py
    TLE

--- ABC199/C - IPFL.py
+++ ABC199/C - IPFL.py
@@ -2,12 +2,16 @@ n = int(input())
 s = list(input())
 q = int(input())
 tab = [list(map(int,input().split())) for _ in range(q)]
-def main():
-  for i in range(q):
-    a = tab[i][1]
-    b = tab[i][2]
-    if(tab[i][0] == 1):s[a-1],s[b-1] = s[b-1],s[a-1]
-    elif(tab[i][0] == 2):s[:n],s[n:] = s[n:],s[:n]
-  print("".join(s))
 
+def main():
+  dict = {}
+  for i in range(len(s)):
+    dict[str(i+1)] = s[i]
+#  print(dict)
+  for j in range(q):
+    if(tab[j][0]==1):dict[str(tab[j][1])],dict[str(tab[j][2])]=dict[str(tab[j][2])],dict[str(tab[j][1])]
+    elif(tab[j][0] == 2):
+      for k in range(n):
+        dict[str(k+1)],dict[str(k+1+n)]=dict[str(k+1+n)],dict[str(k+1)]
+  print("".join(dict.values()))
 main()
