05c7925ee86078bffc00b692fe552ff556407440
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2021-05-09 00:17:15 +0900

    Update C - Ringo's Favorite Numbers 2.py

--- ABC200/C - Ringo's Favorite Numbers 2.py
+++ ABC200/C - Ringo's Favorite Numbers 2.py
@@ -1,9 +1,12 @@
 n = int(input())
 a = list(map(int,input().split()))
 ans = 0
-for i in range(n-1):
-  for j in range(i+1,n):
-    if(str(a[i])[-1] != str(a[j])[-1] or str(a[i])[-2] != str(a[j])[-2]):continue
-    
-    if(abs(a[i]-a[j])%200 == 0):ans+=1
-print(ans)
+cnt = [0]*200
+def answer(n):
+  return int(n*(n+1)/2)
+for i in range(n):
+  cnt[a[i]%200] += 1
+for j in cnt:
+  ans += (j-1)*((j-1)+1)/2
+#print(cnt)
+print(int(ans))
