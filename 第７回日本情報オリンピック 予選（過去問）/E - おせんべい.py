a88f0efd1c80ae96e027b0ac128e505331ae6706
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2021-05-26 15:14:34 +0900

    Update E - おせんべい.py

--- 第７回日本情報オリンピック 予選（過去問）/E - おせんべい.py
+++ 第７回日本情報オリンピック 予選（過去問）/E - おせんべい.py
@@ -1,23 +1,40 @@
-r,c = list(map(int,input().split()))
+from itertools import product
+import sys
+input = sys.stdin.readline
+r,c = map(int,input().split())
 a = [list(map(int,input().split())) for _ in range(r)]
+a = list(zip(*a))
 ans = 0
-for i in range(2**r):
-    arr = []
-#  print(i)
-    for j in range(r):
-        if((i >> j)&1):
-            arr.append(j)
-    print(arr)
-    for k in range(r):
-        if k in arr:
-            print("1st:",list(map(lambda x:(x+1)%2,a[k])))
-            a[k] = list(map(lambda x:(x+1)%2,a[k]))
-        else:
-            print(a[k])
-    for i in range(c):
+for i in product((0,1),repeat= r):
+    sum = 0
+#    print(i)
+    for j in a:
         cnt = 0
-        for j in range(r):
-            # print((a[j][i]+1)%2,end = "")
-            if(a[j][i] == 0):cnt+=1
-        if(cnt >= r//2):(a[j][i]+1)%2
-        print(a[j])
+#        print("j",j)
+        for k in range(r):
+#            print("a[k]",j[k],"i[k]",i[k])
+            if(i[k] == j[k]):
+                cnt += 1
+#        print("sum",sum)
+        sum += max(cnt,r-cnt)
+    ans = max(ans,sum)
+print(ans)
+
+
+
+
+# for j in arr:
+#     for i in range(r):
+#         if(j[i]):first[i] = np.logical_xor(a[i],1).astype(np.int)
+#         else:first[i] = a[i]
+#         print(first[i])
+#     print()
+#     #縦方向でひっくり返すAwesome Code
+#     # for col in range(c):
+#     #     for row in range(r):
+#     #         print("xor?",np.logical_xor(a,arr[0]).astype(np.int))
+#     #         print("sum",sum(np.logical_xor(a,arr).astype(np.int)))
+#     #         ans = max(ans, sum(np.logical_xor(a,arr).astype(np.int)))
+
+
+# print("Test",ans)
