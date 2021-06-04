be74f87d098be19432733affe81e72ff8ab12233
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2021-06-04 10:45:27 +0900

    Update E - Transformable Teacher.py
    TLE

--- ABC181/E - Transformable Teacher.py
+++ ABC181/E - Transformable Teacher.py
@@ -1,14 +1,15 @@
-import math
-N = list(map(int,input().split()))
-H = list(map(int,input().split()))
-W = list(map(int,input().split()))
-ans = 100000001
-#pair_list = []
-#print(H)
-for i in W:
-  Pair_list = [i]
-  Pair_list = sorted(Pair_list + H)
-#  print(Pair_list)
-  ans = min(ans, int(math.fabs(sum(Pair_list[0::2])-sum(Pair_list[1::2]))))
+import bisect
+n,m = list(map(int,input().split()))
+h = sorted(list(map(int,input().split())))
+w = list(map(int,input().split()))
+# print(h)
+# print(w)
+ans = float("inf")
+for i in w:
+    tall = []
+    bisect.bisect_left(h,i)
+    tall = sorted(h+[i])
+    # print("tall",sorted(h+[i]))
+    # print(tall[::2],tall[1::2],sum(tall[::2]),sum(tall[1::2]), sum(tall[::2])-sum(tall[1::2]))
+    ans = min(ans, abs(sum(tall[::2])-sum(tall[1::2])))
 print(ans)
-  
