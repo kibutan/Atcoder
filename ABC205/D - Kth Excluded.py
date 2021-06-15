fbac995521b2a94d753d91f42dd5c28aa1f061b1
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2021-06-16 08:39:57 +0900

    Update D - Kth Excluded.py
    WA

--- ABC205/D - Kth Excluded.py
+++ ABC205/D - Kth Excluded.py
@@ -7,11 +7,22 @@ k = [int(input()) for i in range(q)]
 # k_inv = [i for i in range(1,10**18+1)]^[k]
 a_set = set(a)
 # print(k_inv[1:10])
+bias = 0
 def query(n):
-    global a_set
+    global a_set,a,bias
     index = bisect.bisect_right(a,n)
-    if(n + index not in a_set):return n + index
-    else:return query(n+1)
+    index_2 = bisect.bisect_right(a,n+index)
+    ans = 0
+    if(index == index_2):
+        if(bias != 0):
+            ans = n + bias
+            bias = 0
+            return ans
+        else:return n+index
+    else:
+        if(bias == 0):
+            bias = index
+        return query(n+1)
  
 for i in k:
     print(query(i))
