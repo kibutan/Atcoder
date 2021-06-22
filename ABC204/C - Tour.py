63556185088e4c0dca60b1bd816e96e6a9c40670
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2021-06-22 14:59:29 +0900

    Update C - Tour.py

--- ABC204/C - Tour.py
+++ ABC204/C - Tour.py
@@ -1,19 +1,43 @@
-from itertools import product
+import sys
+input = sys.stdin.readline
 n,m = map(int,input().split())
-if m  == 0 : 
-    print(n)
-    exit()
-# print(m)
-ab = [map(int,input().split()) for _ in range(m)]
-a,b = [set(i) for i in zip(*ab)]
-print(type(a))
-print(a)
-print(b)
+# ab = [map(int,input().split()) for _ in range(m)]
 ans = 0
-route = list(product(a,b))
-cnt = len(route)
-print(route)
-print("ab",a^b)
 
-# cnt += a^b
-print(cnt)
+ROUTE = {}
+for i in range(m):
+    a, b = map(int, input().split())
+    if (a in ROUTE) == False:
+        ROUTE[a] = []
+    ROUTE[a].append(b)
+
+sys.setrecursionlimit(10**5)
+
+# from collections import defaultdict
+# adj = defaultdict(set)
+
+# ans = 0
+
+# for a,b in ab:
+#     adj[a].add(b)
+# seen = [0]*(n+1)
+
+# def dfs(pos):
+#     if seen[pos]:return
+#     seen[pos] = 1
+#     if pos not in adj:return
+#     for next_ in adj[pos]:
+#         if not seen[next_]:dfs(next_)
+
+def dfs(pos):
+    if seen[pos]:return
+    seen[pos] = 1
+    if pos not in ROUTE:return
+    for next_ in ROUTE[pos]:
+        if not seen[next_]:dfs(next_)
+
+for i in range(1,n+1):
+    seen = [0]*(n+1)
+    dfs(i)
+    ans += sum(seen)
+print(ans)
