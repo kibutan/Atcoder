7de0211b783dfe3952ca2f3e45c038cecc9dac63
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2022-01-18 16:54:03 +0900

    Update D - Ki.py
    TLE

--- ABC138/D - Ki.py
+++ ABC138/D - Ki.py
@@ -6,25 +6,42 @@ ab = [map(int, input().split()) for i in range(n-1)]
 a,b = [list(i) for i in zip(*ab)]
 px = [map(int, input().split()) for i in range(q)]
 p,x = [list(i) for i in zip(*px)]
-ans = {}
+
+cost = {x: 0 for x in range(1,n+1)}
+ans = {x: 0 for x in range(1,n+1)}
+arrived = {x: 0 for x in range(1,n+1)}
 
 for i in range(1,n+1):
     ans.setdefault(i,0)
 ki = {}
 for i in range(n-1):
+    #双方向グラフを作る
     ki.setdefault(a[i],[]).append(b[i])
-print(ki)
-# print("ans",ans)
+    ki.setdefault(b[i],[]).append(a[i])
+# print(ki)
 
-def add(node,point):
-    if node in ki.keys():
-        ans[node] += point
-        # print("ki no nakamim ga aru",node)
-        for i in ki[node]:
-            add(i,point)
-        # def add(ki no nakami,point)
-    else:ans[node] += point
+# arrived.setdefault(a[i],-1)
 
-for j in range(q):
-    add(p[j],x[j])
-print(ans)
+# print("ans",ans,"arrive",arrived)
+# 両方向に辺をつないで、頂点から準にポイントを割り振り
+def calc_cost(node,parent_cost=0):
+    arrived[node] = True
+    #親からのコストに自分のコストを乗せる、それが自分のコスト合計
+    parent_cost += cost[node]
+    ans[node] = parent_cost
+    for i in ki[node]:
+        #もし、子供がいれば、子供に自分のコストを引き継ぐ。
+        if not arrived[i]:
+            # print("iiiiiiiiiii",i)
+            calc_cost(i,parent_cost)
+        else:continue
+
+# imosの木版。各ノードのプラス成分を登録
+for i in range(q):
+    cost[p[i]] += x[i]
+# print("Hogehoge",ans)
+
+# 入力は木 = すべてつながっているので呼び出しは一回でいい。
+calc_cost(1)
+# print("ans",*ans.values())
+print(*ans.values())
