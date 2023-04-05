f04ef845facd9a35c2e9c78a264b057855d0820c
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2023-04-05 10:35:14 +0900

    Create B - ASCII Art.py

---
+++ ABC294/B - ASCII Art.py
@@ -0,0 +1,7 @@
+h,w = list(map(int,input().split()))
+a = [list(map(int,input().split())) for _ in range(h)]
+for i in range(h):
+  for j in range(w):
+    if(a[i][j] == 0):print(".",end="")
+    else:print(chr(a[i][j]+64),end="")
+  print()
